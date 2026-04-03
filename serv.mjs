7c594e18ddc5d4ec934e2b6e43980d505e4d522a
Joe Eagar <joeedh@gmail.com>
2026-04-02 22:33:39 -0600

    update path.ux submodule ref

--- serv.mjs
+++ serv.mjs
@@ -125,7 +125,7 @@ const serv = http.createServer({
 });
 
 serv.listen(PORT, HOST, () => {
-  console.log("Server listening on", HOST + ":" + PORT); 
+  console.log("Server listening on", `http://${HOST}:${PORT}`); 
 });
 
 
